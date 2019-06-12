69b359ba6df3da75237edb23864ac23f4d013587
Jane Djumerkoski <jane.djumerkoski@musala.com>
2019-06-12 16:53:13 +0200

    test react app

---
+++ precache-manifest.5be0a4785dc261b31c0a67adfe7c0f5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed24e4f11b8400c9e12eb2fa514865b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f45255dbf6e3b9795de",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0f45255dbf6e3b9795de",
+    "url": "/static/js/main.2fc9710f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
